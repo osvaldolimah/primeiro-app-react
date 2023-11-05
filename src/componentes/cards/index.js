a3f82b4a7aa3b380f4acdb93e72d280cac5c2310
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-04 22:11:23 -0300

    primeiro app em react

---
+++ src/componentes/cards/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Card = () => {
+    return (
+<div className='card-container'>
+    <p>Componentes:</p>
+    <p>Facilita em manter partes menores funcionando corretamente</p>
+    <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
+</div>
+);
+}
+
+export default Card;
---
+++ src/componentes/homePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../cards'
+import './style.css'
+
+export default function HomePage() {
+return (
+    <div className='home-page'>
+    <Card />
+    <Card />
+    <Card />
+    </div>
+);
+}
+
